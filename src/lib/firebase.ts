9bcc5789212a88a3ba3b45fb2764c39ebf04a75f
cyntaxinc <hosting@cyntax.cloud>
2025-10-08 11:58:58 -0400

    Try not-found file for build error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,11 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
 };
 
 
